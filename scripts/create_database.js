8352c57ee3fe47f7017b426a6402be3f8332986b
PhamToanDoMath <phamtoan79125@gmail.com>
2021-11-01 21:35:47 +0700

    Fix create_database

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -7,16 +7,16 @@ var dbconfig = require('../config/database');
 
 var connection = mysql.createConnection(dbconfig.connection);
 
-// connection.query('CREATE DATABASE ' + dbconfig.database);
+connection.query('CREATE DATABASE ' + dbconfig.database);
 
 connection.query('\
 CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
-    `username` VARCHAR(20) NOT NULL, \
+    `email` VARCHAR(40) NOT NULL, \
     `password` CHAR(60) NOT NULL, \
         PRIMARY KEY (`id`), \
     UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
-    UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
+    UNIQUE INDEX `email_UNIQUE` (`email` ASC) \
 )');
 
 console.log('Success: Database Created!')
