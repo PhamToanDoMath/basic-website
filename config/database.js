a9ee36b9d80a174e7a152726ded0f39b1b010e30
PhamToanDoMath <phamtoan79125@gmail.com>
2021-11-01 19:47:07 +0700

    First commit

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': ''
+    },
+	'database': 'textile_agency',
+    'users_table': 'users'
+};
